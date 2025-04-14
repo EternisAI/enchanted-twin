5db82eac2f8595f643b4443cc9747701ef87f53d
Arthurgallina1 <arthur.gallina@gmail.com>
2025-04-14 16:37:46 -0700

    add graphql basic config, add graphqli fetching and codegen with mock schema

--- app/src/renderer/src/App.tsx
+++ app/src/renderer/src/App.tsx
@@ -1,33 +1,28 @@
 import Versions from './components/Versions'
 import electronLogo from './assets/electron.svg'
+import { ApolloClientProvider } from './graphql/provider'
+import ChatContainer from './components/chat/ChatContainer'
 
 function App(): React.JSX.Element {
   const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
     <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
+      <ApolloClientProvider>
+        <img alt="logo" className="logo" src={electronLogo} />
+        {/* <p className="tip">
         Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
+      </p> */}
+        <div className="actions">
+          <div className="action">
+            <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+              Send IPC
+            </a>
+            <ChatContainer />
+          </div>
         </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
+        <Versions></Versions>
+      </ApolloClientProvider>
     </>
   )
 }
