268e10f9f97108bc9729422967130cf80b6cfc7d
Augustinas Malinauskas <augustinasmal@gmail.com>
2025-04-17 00:16:19 -0700

    fix: add path

--- backend/golang/graph/generated.go
+++ backend/golang/graph/generated.go
@@ -61,6 +61,7 @@ type ComplexityRoot struct {
 		ID        func(childComplexity int) int
 		IsIndexed func(childComplexity int) int
 		Name      func(childComplexity int) int
+		Path      func(childComplexity int) int
 		UpdatedAt func(childComplexity int) int
 	}
 
@@ -202,6 +203,13 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.DataSource.Name(childComplexity), true
+
+	case "DataSource.path":
+		if e.complexity.DataSource.Path == nil {
+			break
+		}
+
+		return e.complexity.DataSource.Path(childComplexity), true
 
 	case "DataSource.updatedAt":
 		if e.complexity.DataSource.UpdatedAt == nil {
@@ -1267,6 +1275,50 @@ func (ec *executionContext) _DataSource_name(ctx context.Context, field graphql.
 }
 
 func (ec *executionContext) fieldContext_DataSource_name(_ context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
+	fc = &graphql.FieldContext{
+		Object:     "DataSource",
+		Field:      field,
+		IsMethod:   false,
+		IsResolver: false,
+		Child: func(ctx context.Context, field graphql.CollectedField) (*graphql.FieldContext, error) {
+			return nil, errors.New("field of type String does not have child fields")
+		},
+	}
+	return fc, nil
+}
+
+func (ec *executionContext) _DataSource_path(ctx context.Context, field graphql.CollectedField, obj *model.DataSource) (ret graphql.Marshaler) {
+	fc, err := ec.fieldContext_DataSource_path(ctx, field)
+	if err != nil {
+		return graphql.Null
+	}
+	ctx = graphql.WithFieldContext(ctx, fc)
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (any, error) {
+		ctx = rctx // use context from middleware stack in children
+		return obj.Path, nil
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(string)
+	fc.Result = res
+	return ec.marshalNString2string(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) fieldContext_DataSource_path(_ context.Context, field graphql.CollectedField) (fc *graphql.FieldContext, err error) {
 	fc = &graphql.FieldContext{
 		Object:     "DataSource",
 		Field:      field,
@@ -2401,6 +2453,8 @@ func (ec *executionContext) fieldContext_Query_getDataSources(_ context.Context,
 				return ec.fieldContext_DataSource_id(ctx, field)
 			case "name":
 				return ec.fieldContext_DataSource_name(ctx, field)
+			case "path":
+				return ec.fieldContext_DataSource_path(ctx, field)
 			case "updatedAt":
 				return ec.fieldContext_DataSource_updatedAt(ctx, field)
 			case "isIndexed":
@@ -5057,6 +5111,11 @@ func (ec *executionContext) _DataSource(ctx context.Context, sel ast.SelectionSe
 			}
 		case "name":
 			out.Values[i] = ec._DataSource_name(ctx, field, obj)
+			if out.Values[i] == graphql.Null {
+				out.Invalids++
+			}
+		case "path":
+			out.Values[i] = ec._DataSource_path(ctx, field, obj)
 			if out.Values[i] == graphql.Null {
 				out.Invalids++
 			}
