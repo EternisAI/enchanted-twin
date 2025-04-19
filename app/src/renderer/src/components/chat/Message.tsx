0ff5721abd9a6fd4052e874695974100405828a6
Arthurgallina1 <arthur.gallina@gmail.com>
2025-04-18 21:34:02 -0300

    add dictory for in progress n done tool, improve layout

--- app/src/renderer/src/components/chat/Message.tsx
+++ app/src/renderer/src/components/chat/Message.tsx
@@ -3,6 +3,8 @@ import { motion } from 'framer-motion'
 import { Markdown } from './Markdown'
 import { cn } from '@renderer/lib/utils'
 import { CheckCircle, LoaderIcon } from 'lucide-react'
+import { TOOL_NAMES } from './config'
+import { Badge } from '../ui/badge'
 
 const messageAnimation = {
   initial: { opacity: 0, y: 20 },
@@ -49,32 +51,9 @@ export function AssistantMessageBubble({ message }: { message: Message }) {
       variants={messageAnimation}
     >
       <div className="bg-gray-100 text-gray-800 rounded-lg px-4 py-2 shadow max-w-md">
-        {message.toolCalls.map((toolCall) => {
-          return (
-            <div
-              key={toolCall.id}
-              className={cn(
-                'flex items-center gap-2',
-                toolCall.isCompleted ? 'text-green-600' : 'text-muted-foreground'
-              )}
-            >
-              {toolCall.isCompleted ? (
-                <>
-                  <CheckCircle className="h-4 w-4" />
-                  <span>Used {toolCall.name}</span>
-                </>
-              ) : (
-                <>
-                  <LoaderIcon className="h-4 w-4 animate-spin" />
-                  <span>Using {toolCall.name}...</span>
-                </>
-              )}
-            </div>
-          )
-        })}
         {message.text && <Markdown>{message.text}</Markdown>}
         {message.imageUrls.length > 0 && (
-          <div className="flex gap-2 mt-2">
+          <div className="flex gap-2 my-2">
             {message.imageUrls.map((url, i) => (
               <img
                 key={i}
@@ -85,6 +64,35 @@ export function AssistantMessageBubble({ message }: { message: Message }) {
             ))}
           </div>
         )}
+
+        <div className="flex flex gap-4 pt-2">
+          {message.toolCalls.map((toolCall) => {
+            const toolNameInProgress = TOOL_NAMES[toolCall.name]?.inProgress || toolCall.name
+            const toolNameCompleted = TOOL_NAMES[toolCall.name]?.completed || toolCall.name
+
+            return (
+              <div
+                key={toolCall.id}
+                className={cn(
+                  'flex items-center gap-2',
+                  toolCall.isCompleted ? 'text-green-600' : 'text-muted-foreground'
+                )}
+              >
+                {toolCall.isCompleted ? (
+                  <Badge className="text-green-600 border-green-500" variant="outline">
+                    <CheckCircle className="h-4 w-4" />
+                    <span>{toolNameCompleted}</span>
+                  </Badge>
+                ) : (
+                  <Badge variant="outline" className="border-gray-300">
+                    <LoaderIcon className="h-4 w-4 animate-spin" />
+                    <span>{toolNameInProgress}...</span>
+                  </Badge>
+                )}
+              </div>
+            )
+          })}
+        </div>
 
         {/* {message.toolResults.length > 0 && (
           <div className="mt-3 bg-green-50 p-2 rounded text-xs text-gray-700 whitespace-pre-wrap">
