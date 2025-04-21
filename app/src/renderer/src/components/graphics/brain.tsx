985a6fbce0fc37ef2a7d22d91b0ddce8e54f1d51
Raymond <li.raymond@columbia.edu>
2025-04-21 15:53:27 -0400

    Brain spline

--- app/src/renderer/src/components/graphics/brain.tsx
+++ app/src/renderer/src/components/graphics/brain.tsx
@@ -1,6 +1,9 @@
 import Spline from '@splinetool/react-spline'
+import brainScene from '../../assets/illustrations/brain-2.splinecode?url'
 
 export function Brain() {
+  return <Spline scene={brainScene} />
+  
+  // Fallback to remote URL if needed
   // return <Spline scene="https://prod.spline.design/g9zmLQllMJGky8gO/scene.splinecode" />
-  return <Spline scene="src/assets/illustrations/brain-2.splinecode" />
 }
