5011445ba335a26eb8f1ad23f43da682454f1c5d
Augustinas Malinauskas <augustinasmal@gmail.com>
2025-04-21 17:55:02 -0700

    fix: env variable fixes

--- app/electron.vite.config.ts
+++ app/electron.vite.config.ts
@@ -1,28 +1,41 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin, loadEnv } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
-export default defineConfig({
-  main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
-    base: './',
-    root: 'src/renderer',
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+function inlineEnvVars(prefix: string, raw: Record<string, string>) {
+  return Object.fromEntries(
+    Object.entries(raw)
+      .filter(([key]) => key.startsWith(prefix))
+      .map(([key, val]) => [`process.env.${key}`, JSON.stringify(val)])
+  )
+}
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('[electron-vite] Loaded ENV:', env)
+  return {
+    main: {
+      plugins: [externalizeDepsPlugin()],
+      define: inlineEnvVars('', env)
     },
-    plugins: [
-      TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
-      react(),
-      tailwindcss()
-    ]
+    preload: {
+      plugins: [externalizeDepsPlugin()]
+    },
+    renderer: {
+      base: './',
+      root: 'src/renderer',
+      resolve: {
+        alias: {
+          '@renderer': resolve('src/renderer/src')
+        }
+      },
+      plugins: [
+        TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+        react(),
+        tailwindcss()
+      ]
+    }
   }
 })
