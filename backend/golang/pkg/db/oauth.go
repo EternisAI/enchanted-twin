43bdb832a414fcfe3579be374969ccdf8b29de35
gustav0 <gustav0@frings.ai>
2025-04-22 12:37:12 -0300

    Add Notion OAuth configuration and clean up whitespace in oauth.go

--- backend/golang/pkg/db/oauth.go
+++ backend/golang/pkg/db/oauth.go
@@ -141,34 +141,39 @@ type OAuthConfig struct {
 	DefaultScope  string `db:"default_scope"`
 }
 
-var (
-	// TODO: The ClientSecret should not be stored in code.
-	oauthConfig = map[string]OAuthConfig{
-		"twitter": {
-			ClientID:      "bEFtUmtyNm1wUFNtRUlqQTdmQmE6MTpjaQ",
-			AuthEndpoint:  "https://twitter.com/i/oauth2/authorize",
-			TokenEndpoint: "https://api.twitter.com/2/oauth2/token",
-			UserEndpoint:  "https://api.twitter.com/2/users/me",
-			DefaultScope:  "tweet.read users.read offline.access",
-		},
-		"google": {
-			ClientID:      "993981911648-vtgfk8g1am6kp36pubo5l46902ua1g4t.apps.googleusercontent.com",
-			ClientSecret:  "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",
-			AuthEndpoint:  "https://accounts.google.com/o/oauth2/v2/auth",
-			TokenEndpoint: "https://oauth2.googleapis.com/token",
-			UserEndpoint:  "https://www.googleapis.com/oauth2/v3/userinfo",
-			DefaultScope:  "openid profile email",
-		},
-		"linkedin": {
-			ClientID:      "779sgzrvca0z5a",
-			ClientSecret:  "WPL_AP1.vfwo58d3MCsGiFht.izlFiA==",
-			AuthEndpoint:  "https://www.linkedin.com/oauth/v2/authorization",
-			TokenEndpoint: "https://www.linkedin.com/oauth/v2/accessToken",
-			UserEndpoint:  "https://api.linkedin.com/v2/me",
-			DefaultScope:  "r_basicprofile",
-		},
-	}
-)
+// TODO: The ClientSecret should not be stored in code.
+var oauthConfig = map[string]OAuthConfig{
+	"twitter": {
+		ClientID:      "bEFtUmtyNm1wUFNtRUlqQTdmQmE6MTpjaQ",
+		AuthEndpoint:  "https://twitter.com/i/oauth2/authorize",
+		TokenEndpoint: "https://api.twitter.com/2/oauth2/token",
+		UserEndpoint:  "https://api.twitter.com/2/users/me",
+		DefaultScope:  "tweet.read users.read offline.access",
+	},
+	"google": {
+		ClientID:      "993981911648-vtgfk8g1am6kp36pubo5l46902ua1g4t.apps.googleusercontent.com",
+		ClientSecret:  "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",
+		AuthEndpoint:  "https://accounts.google.com/o/oauth2/v2/auth",
+		TokenEndpoint: "https://oauth2.googleapis.com/token",
+		UserEndpoint:  "https://www.googleapis.com/oauth2/v3/userinfo",
+		DefaultScope:  "openid profile email",
+	},
+	"linkedin": {
+		ClientID:      "779sgzrvca0z5a",
+		ClientSecret:  "WPL_AP1.vfwo58d3MCsGiFht.izlFiA==",
+		AuthEndpoint:  "https://www.linkedin.com/oauth/v2/authorization",
+		TokenEndpoint: "https://www.linkedin.com/oauth/v2/accessToken",
+		UserEndpoint:  "https://api.linkedin.com/v2/me",
+		DefaultScope:  "r_basicprofile",
+	},
+	"notion": {
+		ClientID:      "1ddd872b-594c-805b-969c-0037fd6f3b58",
+		ClientSecret:  "secret_TKTPtZhVXYMgQuElkplb8759bmBeqXn7jjphv4OexUV",
+		AuthEndpoint:  "https://api.notion.com/v1/oauth/authorize?client_id=1ddd872b-594c-805b-969c-0037fd6f3b58&response_type=code&owner=user&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback",
+		TokenEndpoint: "https://api.notion.com/v1/oauth/token",
+		UserEndpoint:  "https://api.notion.com/v1/users/me",
+	},
+}
 
 // OAuthTokens represents oauth tokens for various providers
 type OAuthTokens struct {
@@ -223,7 +228,6 @@ func (s *Store) GetOAuthTokens(ctx context.Context, provider string) (*OAuthToke
 		FROM oauth_tokens
 		WHERE provider = ?
 	`, provider)
-
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, nil // Provider not found
@@ -277,7 +281,6 @@ func (s *Store) GetAndClearOAuthProviderAndVerifier(ctx context.Context, logger 
         FROM oauth_sessions 
         WHERE state = ?
     `, state)
-
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return "", "", "", fmt.Errorf("no OAuth session found for state '%s'", state)
@@ -298,7 +301,6 @@ func (s *Store) GetAndClearOAuthProviderAndVerifier(ctx context.Context, logger 
         DELETE FROM oauth_sessions
         WHERE state = ?
     `, state)
-
 	if err != nil {
 		return "", "", "", fmt.Errorf("failed to delete session: %w", err)
 	}
@@ -371,7 +373,6 @@ func (s *Store) GetOAuthConfig(ctx context.Context, provider string) (*OAuthConf
 		FROM oauth_providers 
 		WHERE provider = ?
 	`, provider)
-
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, fmt.Errorf("provider '%s' not found", provider)
@@ -407,7 +408,6 @@ func (s *Store) ClearOAuthTokens(ctx context.Context, provider string) error {
 			refresh_token = ''
 		WHERE provider = ?
 	`, provider)
-
 	if err != nil {
 		return fmt.Errorf("failed to clear OAuth tokens for provider '%s': %w", provider, err)
 	}
@@ -428,7 +428,6 @@ func (s *Store) GetOAuthStatus(ctx context.Context) ([]OAuthStatus, error) {
         SELECT provider, expires_at, scope FROM oauth_tokens
         WHERE access_token != '' AND expires_at > ?
     `, time.Now())
-
 	if err != nil {
 		return nil, fmt.Errorf("failed to retrieve providers: %w", err)
 	}
