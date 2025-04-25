68ba5d300d04691de6bd21a5cd41ca546e83f7d4
Augustinas Malinauskas <augustinasmal@gmail.com>
2025-04-24 18:25:55 -0700

    fix: notarize script

---
+++ app/build/notarize.js
@@ -0,0 +1,20 @@
+const { notarize } = require('@electron/notarize')
+
+exports.default = async function notarizeApp(context) {
+  const { appOutDir, electronPlatformName } = context
+  if (electronPlatformName !== 'darwin') return
+
+  const appName = context.packager.appInfo.productFilename
+  console.log('🪄 Running custom notarize script…')
+
+  return notarize({
+    tool: 'notarytool',
+    provider: 'api',
+    appBundleId: 'com.yourcompany.enchanted',
+    appPath: `${appOutDir}/${appName}.app`,
+    ascProvider: process.env.NOTARY_TEAM_ID,
+    appleApiKey: './build/AuthKey.p8',
+    appleApiKeyId: process.env.NOTARY_API_KEY_ID,
+    appleApiIssuer: process.env.NOTARY_API_ISSUER
+  })
+}
