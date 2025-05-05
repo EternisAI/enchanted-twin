e152b377a5acbb973386cbc45d45374e9d3534a5
Arthurgallina1 <arthur.gallina@gmail.com>
2025-05-05 15:52:46 -0300

    add version to home page, add waiting twin response back

--- app/src/renderer/src/App.tsx
+++ app/src/renderer/src/App.tsx
@@ -1,18 +1,28 @@
 import { DataSourcesPanel } from './components/data-sources/DataSourcesPanel'
 import MCPPanel from './components/oauth/MCPPanel'
 import { ScrollArea } from './components/ui/scroll-area'
+import { useEffect, useState } from 'react'
 
-function App(): React.JSX.Element {
+export default function App(): React.JSX.Element {
+  const [version, setVersion] = useState<string>('')
+
+  useEffect(() => {
+    const getVersion = async () => {
+      const appVersion = await window.api.getAppVersion()
+      setVersion(appVersion)
+    }
+    getVersion()
+  }, [])
+
   return (
     <ScrollArea className="h-full w-full">
       <div className="flex flex-col gap-8 flex-1 text-foreground w-full h-full justify-center">
         <div className="flex flex-col gap-8 p-4 max-w-4xl self-center justify-center mx-auto">
           <MCPPanel />
           <DataSourcesPanel showStatus={true} />
+          <div className="text-md text-muted-foreground text-center">Version {version}</div>
         </div>
       </div>
     </ScrollArea>
   )
 }
-
-export default App
