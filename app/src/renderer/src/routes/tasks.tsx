62f65cd0bce766c47b239ec28ef010efcb0782f5
Arthurgallina1 <arthur.gallina@gmail.com>
2025-05-07 14:24:24 -0300

    add all endedat, add output

--- app/src/renderer/src/routes/tasks.tsx
+++ app/src/renderer/src/routes/tasks.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/tasks')({
 
 export default function Tasks() {
   return (
-    <div className="p-6 flex flex-col gap-6 max-w-4xl mx-auto">
+    <div className="p-6 flex flex-col gap-6 w-full md:w-4xl mx-auto">
       <h2 className="text-4xl mb-6">Agent Tasks</h2>
       <AgentTasks />
     </div>
