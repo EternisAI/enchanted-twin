2c85a51ba42e3784af5aaaedcb126397f95e4601
Augustinas Malinauskas <augustinasmal@gmail.com>
2025-05-09 13:07:51 -0700

    fix: kokoro

--- backend/golang/pkg/tts/tts.go
+++ backend/golang/pkg/tts/tts.go
@@ -1,10 +1,40 @@
+// Package tts implements text to speech provider used by the frontend to generate audio from text.
 package tts
 
 import (
 	"context"
 	"io"
+	"net/http"
+
+	"github.com/EternisAI/enchanted-twin/pkg/tts/internal/model"
 )
 
 type TTSProvider interface {
 	Stream(ctx context.Context, text string) (io.ReadCloser, error)
+}
+
+// Kokoro TTS model
+// https://github.com/remsky/Kokoro-FastAPI
+type Kokoro struct {
+	Endpoint string
+	Model    string
+	Voice    string
+}
+
+func (k Kokoro) Stream(ctx context.Context, text string) (io.ReadCloser, error) {
+	req := model.Request{
+		Model:          k.Model,
+		Voice:          k.Voice,
+		Input:          text,
+		ResponseFormat: "mp3",
+		Stream:         true,
+	}
+	httpReq, _ := http.NewRequestWithContext(ctx, http.MethodPost, k.Endpoint, req.Encode())
+	httpReq.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Body, nil
 }
