ac19ce6d6537a5cbea5eb24e27e9f0323413e465
Arthurgallina1 <arthur.gallina@gmail.com>
2025-05-14 14:37:32 -0300

    fix ui to show qr code connection

--- app/src/renderer/src/components/data-sources/DataSourceDialog.tsx
+++ app/src/renderer/src/components/data-sources/DataSourceDialog.tsx
@@ -1,64 +1,20 @@
 import { Clock, ExternalLink, FileUp, Settings } from 'lucide-react'
 import { Button } from '../ui/button'
-import { DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '../ui/dialog'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle
+} from '../ui/dialog'
 import { DataSource, PendingDataSource } from './types'
 import { truncatePath } from './utils'
 import { EXPORT_INSTRUCTIONS } from './export-instructions'
 import { ReactNode, useState } from 'react'
-
-{
-  /* <Dialog open={!!selectedSource} onOpenChange={onClose}>
-<DialogContent className="max-w-3xl">
-  <DialogHeader>
-    <DialogTitle>Add {selectedSource.name} Data</DialogTitle>
-    <DialogDescription className="text-sm text-muted-foreground flex items-center gap-2">
-      <Clock className="h-4 w-4 text-muted-foreground/50" />
-      {EXPORT_INSTRUCTIONS[selectedSource.name]?.timeEstimate}
-    </DialogDescription>
-  </DialogHeader>
-
-  <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
-    <TabsList className={`grid ${customComponent ? 'grid-cols-2' : 'grid-cols-1'} mb-6`}>
-      <TabsTrigger value="file" className="flex py-2 items-center gap-2">
-        <FileUp className="h-4 w-4" />
-        File Upload
-      </TabsTrigger>
-
-      {customComponent && (
-        <TabsTrigger value={customComponent.name} className="flex items-center gap-2">
-          <Settings className="h-4 w-4" />
-          {customComponent.name}
-        </TabsTrigger>
-      )}
-    </TabsList>
-
-    <TabsContent value="file">
-      <StandardFileUpload
-        selectedSource={selectedSource}
-        pendingDataSources={pendingDataSources}
-        onFileSelect={onFileSelect}
-      />
-    </TabsContent>
-
-    {customComponent && (
-      <TabsContent value="QR Code">{customComponent.component}</TabsContent>
-    )}
-  </Tabs>
-
-  <DialogFooter>
-    <Button variant="outline" onClick={onClose}>
-      Cancel
-    </Button>
-    <Button
-      onClick={onAddSource}
-      disabled={!pendingDataSources[selectedSource.name] && activeTab === 'file'}
-    >
-      Add Source
-    </Button>
-  </DialogFooter>
-</DialogContent>
-</Dialog> */
-}
+import { TabsContent } from '../ui/tabs'
+import { TabsList, TabsTrigger } from '../ui/tabs'
+import { Tabs } from '../ui/tabs'
 
 export const DataSourceDialog = ({
   selectedSource,
@@ -85,14 +41,65 @@ export const DataSourceDialog = ({
   if (!selectedSource) return null
 
   return (
+    <Dialog open={!!selectedSource} onOpenChange={onClose}>
+      <DialogContent className="max-w-3xl">
+        <DialogHeader>
+          <DialogTitle>Add {selectedSource.name} Data</DialogTitle>
+          <DialogDescription className="text-sm text-muted-foreground flex items-center gap-2">
+            <Clock className="h-4 w-4 text-muted-foreground/50" />
+            {EXPORT_INSTRUCTIONS[selectedSource.name]?.timeEstimate}
+          </DialogDescription>
+        </DialogHeader>
+
+        <Tabs value={activeTab} onValueChange={setActiveTab} className="w-full">
+          <TabsList className={`grid ${customComponent ? 'grid-cols-2' : 'grid-cols-1'} mb-6`}>
+            <TabsTrigger value="file" className="flex py-2 items-center gap-2">
+              <FileUp className="h-4 w-4" />
+              File Upload
+            </TabsTrigger>
+
+            {customComponent && (
+              <TabsTrigger value={customComponent.name} className="flex items-center gap-2">
+                <Settings className="h-4 w-4" />
+                {customComponent.name}
+              </TabsTrigger>
+            )}
+          </TabsList>
+
+          <TabsContent value="file">
+            <StandardFileUpload
+              selectedSource={selectedSource}
+              pendingDataSources={pendingDataSources}
+              onFileSelect={onFileSelect}
+              onClose={onClose}
+              onAddSource={onAddSource}
+            />
+          </TabsContent>
+
+          {customComponent && (
+            <TabsContent value="QR Code">{customComponent.component}</TabsContent>
+          )}
+        </Tabs>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+const StandardFileUpload = ({
+  selectedSource,
+  pendingDataSources,
+  onFileSelect,
+  onClose,
+  onAddSource
+}: {
+  selectedSource: DataSource
+  pendingDataSources: Record<string, PendingDataSource>
+  onFileSelect: () => void
+  onClose: () => void
+  onAddSource: () => void
+}) => {
+  return (
     <>
-      <DialogHeader>
-        <DialogTitle>Add {selectedSource.name} Data</DialogTitle>
-        <DialogDescription className="text-sm text-muted-foreground flex items-center gap-2">
-          <Clock className="h-4 w-4 text-muted-foreground/50" />
-          {EXPORT_INSTRUCTIONS[selectedSource.name]?.timeEstimate}
-        </DialogDescription>
-      </DialogHeader>
       <div className="space-y-8">
         <div className="space-y-4 py-4">
           <div className="rounded-lg">
@@ -138,7 +145,7 @@ export const DataSourceDialog = ({
           </div>
         </div>
       </div>
-      <DialogFooter>
+      <DialogFooter className="pt-4">
         <Button variant="outline" onClick={onClose}>
           Cancel
         </Button>
@@ -147,63 +154,5 @@ export const DataSourceDialog = ({
         </Button>
       </DialogFooter>
     </>
-  )
-}
-
-const StandardFileUpload = ({
-  selectedSource,
-  pendingDataSources,
-  onFileSelect
-}: {
-  selectedSource: DataSource
-  pendingDataSources: Record<string, PendingDataSource>
-  onFileSelect: () => void
-}) => {
-  return (
-    <div className="space-y-8">
-      <div className="space-y-4 py-4">
-        <div className="rounded-lg">
-          <ol className="space-y-4 flex flex-col gap-3">
-            {EXPORT_INSTRUCTIONS[selectedSource.name]?.steps.map((step, index) => (
-              <li key={index} className="flex gap-3">
-                <div className="flex-shrink-0 w-6 h-6 rounded-full bg-accent flex items-center justify-center text-primary font-medium text-sm">
-                  {index + 1}
-                </div>
-                <div className="flex-1">
-                  <p className="text-sm">{step}</p>
-                </div>
-              </li>
-            ))}
-          </ol>
-          {EXPORT_INSTRUCTIONS[selectedSource.name]?.link && (
-            <Button variant="link" className="mt-4 p-0 h-auto text-primary" asChild>
-              <a
-                href={EXPORT_INSTRUCTIONS[selectedSource.name].link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Open {selectedSource.name} Export Page <ExternalLink className="h-4 w-4" />
-              </a>
-            </Button>
-          )}
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-2 bg-card p-4 rounded-lg dark text-white dark:bg-muted dark:text-muted-foreground">
-        <div className="flex flex-col gap-2">
-          <div className="flex items-center gap-2">
-            <div
-              className="flex-1 h-9 px-3 py-1 flex flex-col justify-center rounded-md border bg-background text-sm"
-              onClick={onFileSelect}
-            >
-              {pendingDataSources[selectedSource.name]?.path
-                ? truncatePath(pendingDataSources[selectedSource.name]?.path)
-                : selectedSource.fileRequirement}
-            </div>
-            <Button onClick={onFileSelect}>Browse</Button>
-          </div>
-        </div>
-      </div>
-    </div>
   )
 }
--- app/src/renderer/src/components/data-sources/DataSourcesPanel.tsx
+++ app/src/renderer/src/components/data-sources/DataSourcesPanel.tsx
@@ -569,14 +569,6 @@ export function DataSourcesPanel({
         </div>
       )}
 
-      {/* <DataSourceDialog
-        selectedSource={selectedSource}
-        onClose={() => setSelectedSource(null)}
-        pendingDataSources={pendingDataSources}
-        onFileSelect={handleFileSelect}
-        onAddSource={handleAddSource}
-        customComponent={selectedSource?.customView ? selectedSource.customView : undefined}
-      /> */}
       <Dialog open={!!selectedSource} onOpenChange={() => setSelectedSource(null)}>
         <DialogContent className="fixed left-[50%] top-[50%] z-[200] grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-center data-[state=open]:slide-in-from-center sm:rounded-lg">
           <DataSourceDialog
@@ -585,6 +577,7 @@ export function DataSourcesPanel({
             pendingDataSources={pendingDataSources}
             onFileSelect={handleFileSelect}
             onAddSource={handleAddSource}
+            customComponent={selectedSource?.customView ? selectedSource.customView : undefined}
           />
         </DialogContent>
       </Dialog>
