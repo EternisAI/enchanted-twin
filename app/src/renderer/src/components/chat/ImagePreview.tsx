5f3caeee6318e3bebfeb34b7fe51d69e6c35a6b2
gluonfield <augustinasmal@gmail.com>
2025-05-16 16:49:29 -0700

    Fix image preview accessibility and layout

---
+++ app/src/renderer/src/components/chat/ImagePreview.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogTitle,
+  DialogDescription
+} from '../ui/dialog'
+import { cn } from '@renderer/lib/utils'
+
+interface ImagePreviewProps {
+  src: string
+  alt?: string
+  thumbClassName?: string
+  className?: string
+}
+
+export default function ImagePreview({ src, alt, thumbClassName, className }: ImagePreviewProps) {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <img
+          src={src}
+          alt={alt}
+          className={cn('cursor-zoom-in', thumbClassName)}
+        />
+      </DialogTrigger>
+      <DialogContent
+        className={cn(
+          'p-0 bg-transparent border-none w-fit max-w-none rounded-xl',
+          className
+        )}
+      >
+        <DialogTitle className="sr-only">Image preview</DialogTitle>
+        <DialogDescription className="sr-only">
+          Enlarged view of the selected image
+        </DialogDescription>
+        <img
+          src={src}
+          alt={alt}
+          className="max-h-[80vh] max-w-[80vw] object-contain rounded-xl"
+        />
+      </DialogContent>
+    </Dialog>
+  )
+}
