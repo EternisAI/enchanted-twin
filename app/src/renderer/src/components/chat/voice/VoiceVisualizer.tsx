947d95b698f15988e0c64e05e70da9a8aafe6363
Arthurgallina1 <arthur.gallina@gmail.com>
2025-05-20 17:39:13 -0300

    rm from sidebar, make it togglable, add store to make it easily acessible

--- app/src/renderer/src/components/chat/voice/VoiceVisualizer.tsx
+++ app/src/renderer/src/components/chat/voice/VoiceVisualizer.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef } from 'react'
+import { useMemo, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import { OrbitControls } from '@react-three/drei'
