f337c2630486521ee8d7730f3a88ad70d31f555d
agallina <43250820+Arthurgallina1@users.noreply.github.com>
2025-05-22 20:33:22 -0300

    make right sidebar float and overlap with max height to not (#192)

--- app/src/renderer/src/components/chat/voice/VoiceVisualizer.tsx
+++ app/src/renderer/src/components/chat/voice/VoiceVisualizer.tsx
@@ -56,7 +56,7 @@ export default function VoiceVisualizer({
       </Canvas>
 
       {assistantTextMessage && (
-        <div className="absolute bottom-28 left-1/2 -translate-x-1/2 text-center text-primary text-md">
+        <div className="absolute bottom-28 left-1/2 -translate-x-1/2 max-w-xl text-center text-primary text-md overflow-hidden">
           {assistantTextMessage}
         </div>
       )}
