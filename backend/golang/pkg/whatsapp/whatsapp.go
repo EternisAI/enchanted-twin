63aa05c265439a30e7ec4e19c47d338496c6c86f
gustav0 <gustav0@frings.ai>
2025-05-23 22:57:15 -0300

    refactor: move WhatsApp client bootstrap logic to whatsapp.go

--- backend/golang/pkg/whatsapp/whatsapp.go
+++ backend/golang/pkg/whatsapp/whatsapp.go
@@ -5,13 +5,19 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/charmbracelet/log"
 	"github.com/nats-io/nats.go"
 	"github.com/samber/lo"
+	"go.mau.fi/whatsmeow"
+	"go.mau.fi/whatsmeow/store/sqlstore"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 
@@ -538,4 +544,120 @@ func EventHandler(memoryStorage memory.Storage, logger *log.Logger, nc *nats.Con
 			logger.Info("Received unhandled event", "event", evt)
 		}
 	}
+}
+
+func BootstrapWhatsAppClient(memoryStorage memory.Storage, logger *log.Logger, nc *nats.Conn, dbPath string) *whatsmeow.Client {
+	dbLog := &WhatsmeowLoggerAdapter{Logger: logger, Module: "Database"}
+
+	dbDir := filepath.Dir(dbPath)
+	if dbDir != "." {
+		if err := os.MkdirAll(dbDir, 0o755); err != nil {
+			logger.Error("Failed to create WhatsApp database directory", "error", err)
+			panic(err)
+		}
+	}
+
+	dbFilePath := filepath.Join(dbDir, "whatsapp_store.db")
+	container, err := sqlstore.New("sqlite3", "file:"+dbFilePath+"?_foreign_keys=on", dbLog)
+	if err != nil {
+		logger.Info("Failed to create WhatsApp database:", "dbFilePath", dbFilePath)
+		logger.Error("Failed to create WhatsApp database:", "error", err, "dbFilePath", dbFilePath)
+		panic(err)
+	}
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := &WhatsmeowLoggerAdapter{Logger: logger, Module: "Client"}
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+	client.AddEventHandler(EventHandler(memoryStorage, logger, nc))
+
+	logger.Info("Waiting for WhatsApp connection signal...")
+	connectChan := GetConnectChannel()
+	<-connectChan
+	logger.Info("Received signal to start WhatsApp connection")
+
+	if client.Store.ID == nil {
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			logger.Error("Error connecting to WhatsApp", "error", err)
+		}
+		for evt := range qrChan {
+			switch evt.Event {
+			case "code":
+				qrEvent := QRCodeEvent{
+					Event: evt.Event,
+					Code:  evt.Code,
+				}
+				SetLatestQREvent(qrEvent)
+				whatsappQRChan := GetQRChannel()
+				select {
+				case whatsappQRChan <- qrEvent:
+				default:
+					logger.Warn("Warning: QR channel buffer full, dropping event")
+				}
+				logger.Info("Received new WhatsApp QR code", "qr_code", evt.Code)
+			case "success":
+				qrEvent := QRCodeEvent{
+					Event: "success",
+					Code:  "",
+				}
+				SetLatestQREvent(qrEvent)
+				GetQRChannel() <- qrEvent
+				logger.Info("WhatsApp connection successful")
+
+				StartSync()
+				UpdateSyncStatus(SyncStatus{
+					IsSyncing:      true,
+					IsCompleted:    false,
+					ProcessedItems: 0,
+					TotalItems:     0,
+					StatusMessage:  "Waiting for history sync to begin",
+				})
+				err = PublishSyncStatus(nc, logger)
+				if err != nil {
+					logger.Error("Error publishing sync status", "error", err)
+				}
+
+			default:
+				logger.Info("Login event", "event", evt.Event)
+			}
+		}
+	} else {
+		err = client.Connect()
+		if err != nil {
+			logger.Error("Error connecting to WhatsApp", "error", err)
+		} else {
+			qrEvent := QRCodeEvent{
+				Event: "success",
+				Code:  "",
+			}
+			SetLatestQREvent(qrEvent)
+			GetQRChannel() <- qrEvent
+			logger.Info("Already logged in, reusing session")
+
+			StartSync()
+			UpdateSyncStatus(SyncStatus{
+				IsSyncing:      true,
+				IsCompleted:    false,
+				ProcessedItems: 0,
+				TotalItems:     0,
+				StatusMessage:  "Waiting for history sync to begin",
+			})
+			err = PublishSyncStatus(nc, logger)
+			if err != nil {
+				logger.Error("Error publishing sync status", "error", err)
+			}
+		}
+	}
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
+		client.Disconnect()
+	}()
+
+	return client
 }
