294116a3d7a3289c25baa6f9613bfc4e0f1b50d5
gustav0 <gustav0@frings.ai>
2025-05-23 22:57:20 -0300

    fix: update WhatsApp client bootstrap function reference

--- backend/golang/cmd/server/main.go
+++ backend/golang/cmd/server/main.go
@@ -212,7 +212,7 @@ func main() {
 
 	whatsappClientChan := make(chan *whatsmeow.Client)
 	go func() {
-		client := bootstrap.BootstrapWhatsAppClient(mem, logger, nc, envs.DBPath)
+		client := whatsapp.BootstrapWhatsAppClient(mem, logger, nc, envs.DBPath)
 		whatsappClientChan <- client
 	}()
 
