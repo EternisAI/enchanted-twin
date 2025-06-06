6e190a21e75ab2a2f747aba9012dc9f1eb0a0178
Arthurgallina1 <arthur.gallina@gmail.com>
2025-06-06 15:23:00 -0300

    add thread detailt to chat in case it is present on chat

--- app/src/renderer/src/components/holon/HolonThreadDetail.tsx
+++ app/src/renderer/src/components/holon/HolonThreadDetail.tsx
@@ -64,7 +64,7 @@ export default function HolonThreadDetail({ thread }: HolonThreadDetailProps) {
   )
 
   const handleBack = () => {
-    navigate({ to: '/holon' })
+    router.history.back()
   }
 
   const handleActionClick = (action: string) => {
