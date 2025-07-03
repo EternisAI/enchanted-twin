369c75f6a5103353d9afa038304212a83f4b3c45
jckhoe <ongjackhoe@gmail.com>
2025-07-04 02:35:59 +0900

    Update cli to support image path

--- backend/golang/pkg/coreml/service_test.go
+++ backend/golang/pkg/coreml/service_test.go
@@ -7,9 +7,10 @@ import (
 func TestCoreMLInference(t *testing.T) {
 	binaryPath := "./coreml-cli"
 	modelPath := "./test_coreml"
+	input := "1"
 	service := NewService(binaryPath, modelPath)
 
-	result, err := service.Infer("1234")
+	result, err := service.Infer(input)
 	if err != nil {
 		t.Logf("Error: %v", err)
 	}
