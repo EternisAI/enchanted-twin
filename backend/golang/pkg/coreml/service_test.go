78960ae1f7501cffbe384346b36c01da4f3f3e26
jckhoe <ongjackhoe@gmail.com>
2025-07-04 03:40:49 +0900

    add benchmark function to coreml service

--- backend/golang/pkg/coreml/service_test.go
+++ backend/golang/pkg/coreml/service_test.go
@@ -16,3 +16,18 @@ func TestCoreMLInference(t *testing.T) {
 	}
 	t.Logf("Result: %s", result)
 }
+
+func BenchmarkCoreMLInference(b *testing.B) {
+	binaryPath := "./coreml-cli"
+	modelPath := "./test_coreml"
+	input := "1"
+	service := NewService(binaryPath, modelPath)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := service.Infer(input)
+		if err != nil {
+			b.Fatalf("Error: %v", err)
+		}
+	}
+}
