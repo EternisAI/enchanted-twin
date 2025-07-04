97db07394faa2ef93175789c91c6ba69cf5a2665
gustav0 <gustav0@frings.ai>
2025-07-04 15:41:11 -0300

    lint

--- backend/golang/pkg/dataprocessing/telegram/telegram.go
+++ backend/golang/pkg/dataprocessing/telegram/telegram.go
@@ -238,7 +238,6 @@ func (s *TelegramProcessor) ProcessFile(ctx context.Context, filepath string) ([
 	const progressInterval = 10
 
 	for chatIndex, chat := range telegramData.Chats.List {
-
 		chatId := strconv.Itoa(chat.ID)
 		chatMessageCount := len(chat.Messages)
 
