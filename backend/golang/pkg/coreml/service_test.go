1333e279bd1a252e5cfe88f5788db2396a90f488
jckhoe <ongjackhoe@gmail.com>
2025-07-05 00:54:04 +0900

    Add coreml cli v2 for interactive to preload model

--- backend/golang/pkg/coreml/service_test.go
+++ backend/golang/pkg/coreml/service_test.go
@@ -2,26 +2,85 @@ package coreml
 
 import (
 	"testing"
+	"time"
 )
 
 func TestCoreMLInference(t *testing.T) {
-	binaryPath := "./coreml-cli"
+	binaryPath := "./coreml-cli-v2"
 	modelPath := "./test_coreml"
-	input := "1"
-	service := NewService(binaryPath, modelPath)
+	input := "Testing"
+	service := NewService(binaryPath, modelPath, false)
 
-	result, err := service.Infer(input)
+	start := time.Now()
+	_, err := service.Infer(input)
+	elapsed := time.Since(start)
+
 	if err != nil {
 		t.Logf("Error: %v", err)
 	}
-	t.Logf("Result: %s", result)
+	// t.Logf("Result: %s", result)
+	t.Logf("Inference time: %v", elapsed)
 }
 
 func BenchmarkCoreMLInference(b *testing.B) {
 	binaryPath := "./coreml-cli"
 	modelPath := "./test_coreml"
-	input := "1"
-	service := NewService(binaryPath, modelPath)
+	input := "./cat.jpg"
+	service := NewService(binaryPath, modelPath, false)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := service.Infer(input)
+		if err != nil {
+			b.Fatalf("Error: %v", err)
+		}
+	}
+}
+
+func TestCoreMLInteractiveMode(t *testing.T) {
+	binaryPath := "./coreml-cli-v2"
+	modelPath := "./test_coreml"
+	input := "Testing"
+	service := NewService(binaryPath, modelPath, true)
+	defer service.Close()
+
+	start := time.Now()
+	_, err := service.Infer(input)
+	elapsed := time.Since(start)
+
+	if err != nil {
+		t.Logf("Error: %v", err)
+	}
+	// t.Logf("Result: %s", result)
+	t.Logf("Interactive inference time: %v", elapsed)
+}
+
+func TestCoreMLInteractiveMultipleInferences(t *testing.T) {
+	binaryPath := "./coreml-cli-v2"
+	modelPath := "./test_coreml"
+	service := NewService(binaryPath, modelPath, true)
+	defer service.Close()
+
+	inputs := []string{"Testing1", "Testing2", "Testing3"}
+
+	for i, input := range inputs {
+		start := time.Now()
+		_, err := service.Infer(input)
+		elapsed := time.Since(start)
+
+		if err != nil {
+			t.Logf("Error on inference %d: %v", i+1, err)
+		}
+		t.Logf("Interactive inference %d time: %v", i+1, elapsed)
+	}
+}
+
+func BenchmarkCoreMLInteractiveMode(b *testing.B) {
+	binaryPath := "./coreml-cli"
+	modelPath := "./test_coreml"
+	input := "test"
+	service := NewService(binaryPath, modelPath, true)
+	defer service.Close()
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
