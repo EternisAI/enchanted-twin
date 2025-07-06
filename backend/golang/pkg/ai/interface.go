1616456f8a2069d2d4aad6049c216ebbf0eecd1a
jckhoe <ongjackhoe@gmail.com>
2025-07-07 03:21:07 +0900

    Add interface for Completions and Embeddings

---
+++ backend/golang/pkg/ai/interface.go
@@ -0,0 +1,14 @@
+package ai
+
+import (
+	"context"
+
+	"github.com/openai/openai-go"
+)
+
+type AIService interface {
+	Completions(ctx context.Context, messages []openai.ChatCompletionMessageParamUnion, tools []openai.ChatCompletionToolParam, model string) (openai.ChatCompletionMessage, error)
+	ParamsCompletions(ctx context.Context, params openai.ChatCompletionNewParams) (openai.ChatCompletionMessage, error)
+	Embeddings(ctx context.Context, inputs []string, model string) ([][]float64, error)
+	Embedding(ctx context.Context, input string, model string) ([]float64, error)
+}
