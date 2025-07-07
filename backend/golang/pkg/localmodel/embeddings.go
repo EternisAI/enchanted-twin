f23bb1c12478b6c56c9fc49fca920bfdaf5077bc
Jason Lee <lwlee2608@gmail.com>
2025-07-08 00:16:59 +0800

    add Embeddings method to interface

--- backend/golang/pkg/localmodel/embeddings.go
+++ backend/golang/pkg/localmodel/embeddings.go
@@ -2,6 +2,7 @@ package localmodel
 
 import "context"
 
-type EmbeddingsModel interface {
+type EmbeddingModel interface {
 	Embedding(ctx context.Context, input string) ([]float32, error)
+	Embeddings(ctx context.Context, inputs []string) ([][]float32, error)
 }
--- backend/golang/pkg/localmodel/jinaaiembedding/model.go
+++ backend/golang/pkg/localmodel/jinaaiembedding/model.go
@@ -1,6 +1,12 @@
 package jinaaiembedding
 
-import "context"
+import (
+	"context"
+
+	"github.com/EternisAI/enchanted-twin/pkg/localmodel"
+)
+
+var _ localmodel.EmbeddingModel = (*JinaAIEmbeddingModel)(nil)
 
 type JinaAIEmbeddingModel struct {
 	tokenizer *SentencePieceTokenizer
@@ -13,5 +19,9 @@ func NewJinaAIEmbeddingModel() *JinaAIEmbeddingModel {
 }
 
 func (m *JinaAIEmbeddingModel) Embedding(ctx context.Context, input string) ([]float32, error) {
+	return nil, nil
+}
+
+func (m *JinaAIEmbeddingModel) Embeddings(ctx context.Context, inputs []string) ([][]float32, error) {
 	return nil, nil
 }
