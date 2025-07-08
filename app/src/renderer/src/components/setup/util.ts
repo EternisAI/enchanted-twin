73418d6e8643955f53e32b9a18b8a648692a5e83
Arthurgallina1 <arthur.gallina@gmail.com>
2025-07-08 18:58:41 -0300

    clean code with reusable fn

--- app/src/renderer/src/components/setup/util.ts
+++ app/src/renderer/src/components/setup/util.ts
@@ -1,3 +1,13 @@
+export function formatBytes(bytes: number): string {
+  if (bytes === 0) return '0 B'
+
+  const k = 1024
+  const sizes = ['B', 'KB', 'MB', 'GB']
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(1))}${sizes[i]}`
+}
+
 export const initialDownloadState = {
   embeddings: {
     downloading: false,
@@ -20,14 +30,4 @@ export const initialDownloadState = {
     totalBytes: 0,
     downloadedBytes: 0
   }
-}
-
-export function formatBytes(bytes: number): string {
-  if (bytes === 0) return '0 B'
-
-  const k = 1024
-  const sizes = ['B', 'KB', 'MB', 'GB']
-  const i = Math.floor(Math.log(bytes) / Math.log(k))
-
-  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(1))}${sizes[i]}`
 }
