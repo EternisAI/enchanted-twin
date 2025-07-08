4376ae56b760e9ef70075e392d97f3f39372d730
Jason Lee <lwlee2608@gmail.com>
2025-07-08 23:10:55 +0800

    add onnxruntime integration

--- backend/golang/pkg/localmodel/jinaaiembedding/model.go
+++ backend/golang/pkg/localmodel/jinaaiembedding/model.go
@@ -3,6 +3,8 @@ package jinaaiembedding
 import (
 	"context"
 
+	ort "github.com/yalue/onnxruntime_go"
+
 	"github.com/EternisAI/enchanted-twin/pkg/localmodel"
 )
 
@@ -10,12 +12,36 @@ var _ localmodel.EmbeddingModel = (*JinaAIEmbeddingModel)(nil)
 
 type JinaAIEmbeddingModel struct {
 	tokenizer *SentencePieceTokenizer
+	session   *ort.DynamicAdvancedSession
 }
 
-func NewJinaAIEmbeddingModel() *JinaAIEmbeddingModel {
+func NewJinaAIEmbeddingModel(modelPath string) (*JinaAIEmbeddingModel, error) {
+	// TODO
+	// ort.SetSharedLibraryPath("/usr/local/lib/onnxruntime/libonnxruntime.dylib")
+
+	err := ort.InitializeEnvironment()
+	if err != nil {
+		return nil, err
+	}
+
+	session, err := ort.NewDynamicAdvancedSession(modelPath,
+		[]string{"input_ids", "attention_mask", "token_type_ids"},
+		[]string{"last_hidden_state"}, nil)
+	if err != nil {
+		return nil, err
+	}
+
 	return &JinaAIEmbeddingModel{
 		tokenizer: NewSentencePieceTokenizer(),
+		session:   session,
+	}, nil
+}
+
+func (m *JinaAIEmbeddingModel) Close() {
+	if m.session != nil {
+		m.session.Destroy()
 	}
+	ort.DestroyEnvironment()
 }
 
 func (m *JinaAIEmbeddingModel) Embedding(ctx context.Context, input string) ([]float32, error) {
