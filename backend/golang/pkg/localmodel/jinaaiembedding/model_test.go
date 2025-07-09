9066be66f3cd11201d8b9fad3bd8295436b1096d
Jason Lee <lwlee2608@gmail.com>
2025-07-09 12:43:37 +0800

    take APP_DATA_PATH as argument for localmodel

--- backend/golang/pkg/localmodel/jinaaiembedding/model_test.go
+++ backend/golang/pkg/localmodel/jinaaiembedding/model_test.go
@@ -2,6 +2,7 @@ package jinaaiembedding
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,10 +11,13 @@ import (
 func TestJinaAIEmbeddingModel(t *testing.T) {
 	t.Skip()
 
-	modelDir := "model/"
-	onnxLib := "/usr/local/lib/onnxruntime/lib/libonnxruntime.so"
+	appDataPath := os.Getenv("APP_DATA_PATH")
+	assert.NotEmpty(t, appDataPath)
 
-	model, err := NewJinaAIEmbeddingModel(modelDir, onnxLib)
+	sharedLibPath := os.Getenv("SHARED_LIBRARY_PATH")
+	assert.NotEmpty(t, sharedLibPath)
+
+	model, err := NewJinaAIEmbeddingModel(appDataPath, sharedLibPath)
 	assert.NoError(t, err)
 	defer model.Close()
 
