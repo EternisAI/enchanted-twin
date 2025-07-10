1c81ff46b0c6bb1c0f48a336e6f71d1669c5a638
Arthurgallina1 <arthur.gallina@gmail.com>
2025-07-10 16:44:24 -0300

    refactor dependency handling to be more dynamic, add new dep

--- backend/golang/pkg/auth/oauth.go
+++ backend/golang/pkg/auth/oauth.go
@@ -652,7 +652,7 @@ func IsWhitelisted(ctx context.Context, logger *log.Logger, store *db.Store) (bo
 	}
 
 	var whitelistResp struct {
-		Email       string `json:"email"`
+		UserID      string `json:"userID"`
 		Whitelisted bool   `json:"whitelisted"`
 	}
 
@@ -667,7 +667,7 @@ func IsWhitelisted(ctx context.Context, logger *log.Logger, store *db.Store) (bo
 		logger.Error("failed to close whitelist response body", "error", closeErr)
 	}
 
-	logger.Debug("whitelist check result", "email", whitelistResp.Email, "whitelisted", whitelistResp.Whitelisted)
+	logger.Debug("whitelist check result", "user", whitelistResp.UserID, "whitelisted", whitelistResp.Whitelisted)
 
 	if whitelistResp.Whitelisted {
 		return true, nil
