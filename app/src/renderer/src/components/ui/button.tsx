f84d5623c6e02751f31a0e65224e9722f99c122d
Artur Pokusin <gh@pokusin.com>
2025-07-14 13:23:41 -0700

    feat: add use-sound library and implement audio feedback in button component

--- app/src/renderer/src/components/ui/button.tsx
+++ app/src/renderer/src/components/ui/button.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@renderer/lib/utils'
+import { useSound } from 'use-sound'
+import clickSound from '@renderer/assets/audio/ui/button-glassy.mp3'
 
 const buttonVariants = cva(
   "cursor-pointer relative active:scale-[98%] hover:shadow-xl active:shadow-sm hover:bg-card transition-all duration-200 ease-in-out inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-base font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
@@ -37,18 +39,29 @@ function Button({
   variant,
   size,
   asChild = false,
+  sound = true,
+  onClick,
   ...props
 }: React.ComponentProps<'button'> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean
+    sound?: boolean
+    onClick?: () => void
   }) {
   const Comp = asChild ? Slot : 'button'
-
+  const [play] = useSound(clickSound, { volume: 0.5 })
+  const handleClick = () => {
+    if (sound) {
+      play()
+    }
+    onClick?.()
+  }
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
+      onClick={handleClick}
     />
   )
 }
