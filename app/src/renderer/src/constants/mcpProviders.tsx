e91dc3ecf914a89b2406114e5568737d89e7715a
Artur Pokusin <gh@pokusin.com>
2025-07-16 17:38:51 -0700

    style: simplify icon rendering in PROVIDER_CONFIG by removing unnecessary IconContainer wrappers

--- app/src/renderer/src/constants/mcpProviders.tsx
+++ app/src/renderer/src/constants/mcpProviders.tsx
@@ -6,7 +6,6 @@ import IconContainer from '@renderer/assets/icons/IconContainer'
 import { PlugIcon } from 'lucide-react'
 import screenpipeIcon from '@renderer/assets/icons/screenpipe.png'
 import enchantedIcon from '@resources/icon.png'
-import freysaIcon from '@resources/freysa.png'
 
 interface ProviderConfig {
   provider: string
@@ -22,11 +21,7 @@ export const PROVIDER_CONFIG: Record<McpServerType, ProviderConfig> = {
     scope:
       'openid email profile https://www.googleapis.com/auth/drive https://mail.google.com/ https://www.googleapis.com/auth/calendar',
     description: 'Access Gmail, Google Drive, and Calendar',
-    icon: (
-      <IconContainer>
-        <Google className="size-7" />
-      </IconContainer>
-    ),
+    icon: <Google className="size-7" />,
     supportsMultipleConnections: false
   },
   SLACK: {
@@ -34,11 +29,7 @@ export const PROVIDER_CONFIG: Record<McpServerType, ProviderConfig> = {
     scope:
       'channels:read,groups:read,channels:history,groups:history,im:read,mpim:read,search:read,users:read',
     description: 'Read messages and communicate with your team',
-    icon: (
-      <IconContainer>
-        <Slack className="size-7" />
-      </IconContainer>
-    ),
+    icon: <Slack className="size-7" />,
     supportsMultipleConnections: false
   },
   TWITTER: {
@@ -56,11 +47,7 @@ export const PROVIDER_CONFIG: Record<McpServerType, ProviderConfig> = {
     provider: 'screenpipe',
     scope: '',
     description: 'Record screen activity for AI context',
-    icon: (
-      <IconContainer>
-        <img src={screenpipeIcon} alt="Screenpipe" className="size-7" />
-      </IconContainer>
-    ),
+    icon: <img src={screenpipeIcon} alt="Screenpipe" className="size-10" />,
     supportsMultipleConnections: false
   },
   OTHER: {
@@ -78,22 +65,14 @@ export const PROVIDER_CONFIG: Record<McpServerType, ProviderConfig> = {
     provider: 'enchanted',
     scope: '',
     description: 'Generate images and search the web',
-    icon: (
-      <IconContainer>
-        <img src={enchantedIcon} alt="Essentials" className="size-7" />
-      </IconContainer>
-    ),
+    icon: <img src={enchantedIcon} alt="Essentials" className="size-10" />,
     supportsMultipleConnections: false
   },
   FREYSA: {
     provider: 'freysa',
     scope: '',
     description: 'Generate videos with popular templates or create your own',
-    icon: (
-      <IconContainer>
-        <img src={freysaIcon} alt="Freysa" className="size-7" />
-      </IconContainer>
-    ),
+    icon: <img src={enchantedIcon} alt="Freysa" className="size-10" />,
     supportsMultipleConnections: false
   }
 }
