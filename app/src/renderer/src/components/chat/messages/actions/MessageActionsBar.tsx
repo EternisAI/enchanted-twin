025c67c720414b93758a6a28545359eb7c9d2a21
Artur Pokusin <gh@pokusin.com>
2025-07-18 18:58:40 -0700

    style: reduce opacity of MessageActionsBar for improved visual feedback

--- app/src/renderer/src/components/chat/messages/actions/MessageActionsBar.tsx
+++ app/src/renderer/src/components/chat/messages/actions/MessageActionsBar.tsx
@@ -1,6 +1,6 @@
 export function MessageActionsBar({ children }: { children: React.ReactNode }) {
   return (
-    <div className="opacity-50 focus-within:opacity-100 hover:opacity-100 transition-opacity relative -left-2 flex flex-row items-start gap-0 justify-start w-full">
+    <div className="opacity-25 focus-within:opacity-100 hover:opacity-100 transition-opacity relative -left-2 flex flex-row items-start gap-0 justify-start w-full">
       {children}
     </div>
   )
