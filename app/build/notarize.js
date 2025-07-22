e0253d900e307326b1ad04c3315c0162123afa03
agallina <43250820+Arthurgallina1@users.noreply.github.com>
2025-07-22 20:02:55 -0300

    Fix/workflow (#514)
    * add to right place
    
    * rm old
    
    * add new make command, update workflow
    
    * fix makefile command, add dev release on electron builder
    
    * foix pckage

--- app/build/notarize.js
+++ app/build/notarize.js
@@ -5,12 +5,15 @@ exports.default = async function notarizeApp(context) {
   if (electronPlatformName !== 'darwin') return
 
   const appName = context.packager.appInfo.productFilename
+  const appBundleId = context.packager.appInfo.metadata.appId
   console.log('🪄 Running custom notarize script…')
+  console.log(`📦 App: ${appName}`)
+  console.log(`🆔 Bundle ID: ${appBundleId}`)
 
   return notarize({
     tool: 'notarytool',
     provider: 'api',
-    appBundleId: 'com.eternis.enchanted',
+    appBundleId: appBundleId,
     appPath: `${appOutDir}/${appName}.app`,
     ascProvider: process.env.NOTARY_TEAM_ID,
     appleApiKey: './build/AuthKey.p8',
